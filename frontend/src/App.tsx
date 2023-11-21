71fe623186c71ac8ce08a084dbd284aba1047380
Madan <madangowda3242@gmail.com>
2023-11-21 14:11:36 +0530

    ui for search results

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,11 @@ import Navbar from "./components/Navbar"
 import Search from "./components/Search"
 import axios from "axios";
 
+// type responseType = {
+//   busniess_name: string,
+//   pan: string,
+
+// }
 const App = () => {
   const [searchText, setSearchText] = useState("");
   const [response, setResponse] = useState({});
@@ -11,8 +16,9 @@ const App = () => {
     e.preventDefault();
     const API_URL = `${import.meta.env.VITE_API_URL}/search?gst=${searchText.toString()}`;
     const response = await axios.get(`${API_URL}`)
-
-    setResponse(response.data);
+    console.log(response);
+    
+    setResponse(response?.data?.data[0]);
   }
   return (
     <div className="h-screen bg-green-50">
@@ -20,9 +26,22 @@ const App = () => {
       <div>
         <Search setSearchText={setSearchText} searchFunction={searchFunction} />
         <div className="text-center">
-        {`${import.meta.env.VITE_API_URL}/search?gst=${searchText}`}
-        <br />
-        {JSON.stringify(response)}
+        <div className="flex justify-center">
+        <div className="w-1/2 h-80 p-3 bg-white rounded">
+          <h3 className="text-left italic">Search result for: <span className="font-bold">{searchText}</span></h3>
+          <div>
+            {/* {JSON.stringify(response)} */}
+            <h2 className="text-xl"><span className="font-bold">Busniess Name:</span> {response?.busniess_name}</h2>  
+            <h2 className="text-xl"><span className="font-bold">PAN:</span> {response?.pan}</h2>  
+            <h2 className="text-xl"><span className="font-bold">Address:</span> {response?.address}</h2> 
+            <h2 className="text-xl"><span className="font-bold">Pincode:</span> {response?.pincode}</h2> 
+            <h2 className="text-xl"><span className="font-bold">Nature of Busniess:</span> {response?.nature_of_busniess}</h2> 
+            <h2 className="text-xl"><span className="font-bold">Department Code:</span> {response?.department_code}</h2> 
+            <h2 className="text-xl"><span className="font-bold">Registration Type:</span> {response?.registration_type}</h2> 
+            <h2 className="text-xl"><span className="font-bold">Registration date:</span> {response?.registration_date}</h2> 
+          </div>
+        </div>
+        </div>
         </div>
       </div>
     </div>
